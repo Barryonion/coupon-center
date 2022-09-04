02f5d4b1e8c04e5173153cc35c7625bc611ba022
Barryonion <zhangxucheng991@gmail.com>
2022-09-04 23:14:18 +0800

    commit

--- 0-spring-boot-quick-start/coupon-customer-serv/coupon-customer-impl/src/main/java/com/barry/coupon/customer/service/intf/CouponCustomerService.java
+++ 0-spring-boot-quick-start/coupon-customer-serv/coupon-customer-impl/src/main/java/com/barry/coupon/customer/service/intf/CouponCustomerService.java
@@ -15,17 +15,25 @@ import java.util.List;
  */
 public interface CouponCustomerService {
 
-    // 领券接口
+    /**
+     * 领券接口
+     * */
     Coupon requestCoupon(RequestCoupon request);
 
-    // 核销优惠券
+    /**
+     * 核销优惠券
+     * */
     ShoppingCart placeOrder(ShoppingCart info);
 
-    // 优惠券金额试算
+    /**
+     * 优惠券金额试算
+     * */
     SimulationResponse simulateOrderPrice(SimulationOrder order);
 
     void deleteCoupon(Long userId, Long couponId);
 
-    // 查询用户优惠券
+    /**
+     * 查询用户优惠券
+     * */
     List<CouponInfo> findCoupon(SearchCoupon request);
 }
