9ce1a3987aa58a93449b5c5d067fa4a34a287d3e
Barryonion <zhangxucheng991@gmail.com>
2022-09-15 23:28:01 +0800

    commit

--- 1-服务治理和负载均衡Nacos+Loadbalancer/coupon-template-serv/coupon-template-dao/src/main/java/com/geekbang/coupon/template/dao/CouponTemplateDao.java
+++ 1-服务治理和负载均衡Nacos+Loadbalancer/coupon-template-serv/coupon-template-dao/src/main/java/com/geekbang/coupon/template/dao/CouponTemplateDao.java
@@ -14,16 +14,24 @@ import java.util.List;
 public interface CouponTemplateDao
         extends JpaRepository<CouponTemplate, Long> {
 
-    // 根据Shop ID查询出所有券模板
+    /**
+     * 根据Shop ID查询出所有券模板
+     */
     List<CouponTemplate> findAllByShopId(Long shopId);
 
-    // IN查询 + 分页支持的语法
+    /**
+     * IN查询 + 分页支持的语法
+     */
     Page<CouponTemplate> findAllByIdIn(List<Long> Id, Pageable page);
 
-    // 根据shop ID + 可用状态查询店铺有多少券模板
+    /**
+     * 根据shop ID + 可用状态查询店铺有多少券模板
+     */
     Integer countByShopIdAndAvailable(Long shopId, Boolean available);
 
-    // 将优惠券设置为不可用
+    /**
+     * 将优惠券设置为不可用
+     */
     @Modifying
     @Query("update CouponTemplate c set c.available = 0 where c.id = :id")
     int makeCouponUnavailable(@Param("id") Long id);
