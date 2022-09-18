26750033f788eef4fbb00b8a22464e13c5d5f824
Barryonion <zhangxucheng991@gmail.com>
2022-09-18 23:25:54 +0800

    commit

--- 1-服务治理和负载均衡Nacos+Loadbalancer/coupon-template-serv/coupon-template-dao/src/main/java/com/geekbang/coupon/template/dao/converter/RuleConverter.java
+++ 1-服务治理和负载均衡Nacos+Loadbalancer/coupon-template-serv/coupon-template-dao/src/main/java/com/geekbang/coupon/template/dao/converter/RuleConverter.java
@@ -6,6 +6,9 @@ import com.geekbang.coupon.template.api.beans.rules.TemplateRule;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+/**
+ * @author barry
+ */
 @Converter
 public class RuleConverter implements AttributeConverter<TemplateRule, String> {
 
--- 1-服务治理和负载均衡Nacos+Loadbalancer/coupon-template-serv/coupon-template-dao/src/main/java/com/geekbang/coupon/template/dao/entity/CouponTemplate.java
+++ 1-服务治理和负载均衡Nacos+Loadbalancer/coupon-template-serv/coupon-template-dao/src/main/java/com/geekbang/coupon/template/dao/entity/CouponTemplate.java
@@ -32,31 +32,41 @@ public class CouponTemplate implements Serializable {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    // 状态是否可用
+    /**
+     * 状态是否可用
+     */
     @Column(name = "available", nullable = false)
     private Boolean available;
 
     @Column(name = "name", nullable = false)
     private String name;
 
-    // 适用门店-如果为空，则为全店满减券
+    /**
+     * 适用门店-如果为空，则为全店满减券
+     */
     @Column(name = "shop_id")
     private Long shopId;
 
     @Column(name = "description", nullable = false)
     private String description;
 
-    // 优惠券类型
+    /**
+     * 优惠券类型
+     */
     @Column(name = "type", nullable = false)
     @Convert(converter = CouponTypeConverter.class)
     private CouponType category;
 
-    // 创建时间，通过@CreateDate注解自动填值（需要配合@JpaAuditing注解在启动类上生效）
+    /**
+     * 创建时间，通过@CreateDate注解自动填值（需要配合@JpaAuditing注解在启动类上生效）
+     */
     @CreatedDate
     @Column(name = "created_time", nullable = false)
     private Date createdTime;
 
-    // 优惠券核算规则，平铺成JSON字段
+    /**
+     * 优惠券核算规则，平铺成JSON字段
+     */
     @Column(name = "rule", nullable = false)
     @Convert(converter = RuleConverter.class)
     private TemplateRule rule;
